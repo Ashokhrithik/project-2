43e337c8697c14544381da06a091f10b20df64a2
Ashokhrithik <ashokhrithick@gmail.com>
2021-11-10 17:00:10 +0530

    crysta

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+ private void greensOmr() {
+   System.out.println("Greenstechnology branch-OMR");
+}
+ private void greensAdayar() {
+  System.out.println("Greentechnology branch-Adayar");
+}
+ private void greensTambaram() {
+ System.out.println("Greenstechnlogy branch-Tambaram");
+}
+ private void greensVelacherry() {
+  System.out.println("Greenstechnology branch-Velacherry");
+}
+ private void greensAnnanagar() {
+ System.out.println("Greenstechnology branch-Anna Nagar");
+}
+ public static void main(String[] args) {
+	GreensTech green = new GreensTech();
+	green.greensOmr();
+	green.greensAdayar();
+	green.greensTambaram();
+	green.greensVelacherry();
+	green.greensAnnanagar();
+}
+}
